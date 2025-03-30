63fd10de943e01edfbb122230df9af8857062d96
CespGonzaDev <d.cespedes.3@estudiantec.cr>
2025-03-29 18:32:33 -0600

    se movieron funciones al nuevo modulo

--- UsrManagement.py
+++ UsrManagement.py
@@ -1,9 +1,10 @@
 import json
 import os
 
+
+
 carpeta_usr = "usr" #carpeta para guardar los usuarios
 carpeta_conv = "conv" #carpeta para guardar las conversaciones
-
 
 def cargarArchivosUsr(nombreUsr):
     """Método que carga los archivos que estan en la carpeta de usuarios
@@ -16,44 +17,10 @@ def cargarArchivosUsr(nombreUsr):
     """
     rutaArchivo = os.path.join(carpeta_usr, f"{nombreUsr}_usr.txt")
     if os.path.exists(rutaArchivo):
-        with open(rutaArchivo, "r") as archivo:
+        with open(rutaArchivo, "r",encoding="utf-8") as archivo:
             return json.load(archivo)
     else:
         return None
-
-def cargarArchivosConv(nombreUsr):
-    """Método que carga los archivos que estan en la carpeta de conversaciones
-
-    Args:
-        nombreUsr (str): Nombre de usuario del inicio de sesión
-
-    Returns:
-        list: retorna la lectura de todas las líneas del archivo
-    """
-    rutaArchivo = os.path.join(carpeta_conv, f"{nombreUsr}_conv.txt")
-    if os.path.exists(rutaArchivo):
-        with open(rutaArchivo, "r") as archivo:
-            return archivo.readlines()  # Lee las líneas directamente
-    else:
-        return []  # Retorna una lista vacía si no existe el archivo
-
-def escribirArchivosConv(nombreUsr, mensaje, respuesta):
-    """Método que escribe las conversaciones en los archivos que estan en la carpeta de conversaciones
-    Args:
-        nombreUsr (str): Nombre de usuario del inicio de sesión
-        mensaje (str): Mensaje que se escribio en la conversación
-        respuesta (str): Respuesta que dio chatgpt ante el mensaje
-    """
-    rutaArchivo = os.path.join(carpeta_conv, f"{nombreUsr}_conv.txt")
-    
-    conversaciones = cargarArchivosConv(nombreUsr) # Cargar el historial existente
-
-    conversaciones.append(f"Tú: {mensaje}\nChatGPT: {respuesta}\n") # Agregar el nuevo mensaje y respuesta al historial
-    
-    with open(rutaArchivo, "w", encoding="utf-8") as archivo: # Escribir el historial completo en el archivo
-        archivo.writelines(conversaciones)  # Escribimos todas las líneas de la lista
-
-
 
 def inicioSesion():
     """Método para iniciar sesión en el programa
@@ -70,6 +37,7 @@ def inicioSesion():
          return nombreUsr
     else:
         print("Acceso denegado. Usuario o contraseña incorrectos.")
+
 
 def registroUsusuarios():
     """Método para registrar un nuevo usuario en el programa
@@ -93,27 +61,8 @@ def registroUsusuarios():
     # print(f"Usuario '{nombreUsr}' registrado exitosamente.")
     # print(usuario)
 
-def historialConversaciones(nombreUsr):
-    """Método para mostrar el historial de conversaciones
 
-    Args:
-        nombreUsr (_type_): _description_
-    """
-    usuario = cargarArchivosUsr(nombreUsr)
-    if usuario and usuario["username"] == nombreUsr:
-         print(f"Acceso permitido. ¡Bienvenido al Historial de Conversaciones de {nombreUsr}!")
-    else:
-        print("Usuario no encontrado.")
-        return  
-    
-    conversaciones = cargarArchivosConv(nombreUsr)
 
-    if conversaciones:
-        print("Historial de Conversaciones:")
-        for i, conv in enumerate(conversaciones):  # Recorremos las conversaciones y las mostramos
-            print(f"{i + 1}. {conv.strip()}")  # Mostrar sin los saltos de línea
-    else:
-        print("No hay conversaciones guardadas.")
 
          
     
